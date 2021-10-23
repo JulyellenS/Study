69c3a6e071a8e1f146e8fd7862f46ccd46f9f26f
Julyellen Santos <julyellensantos1818@gmail.com>
2021-10-23 19:16:49 -0300

    primeiro commit

---
+++ src/test/java/com/m4u/study/StudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.m4u.study;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
