f8093873c323434fe3f7f485361003dabb18dc5f
Marco Santana <marcos.sinsan@gmail.com>
2021-10-31 16:01:57 -0300

    Adicionando atributos e metodos na classe Adress.

--- src/main/java/com/m4u/study/model/Adress.java
+++ src/main/java/com/m4u/study/model/Adress.java
@@ -1,5 +1,153 @@
 package com.m4u.study.model;
 
 public class Adress {
+	
+	private Integer idAdress;
+	
+	private String street;
+	
+	private String city;
+	
+	private String state;
+	
+	private Integer zipCode;
+	
+	private String country;
+	
+	private Student idStudent;
+	
+	
+	
+	public Adress() {}
+	
+	public Adress(Integer idAdress, String street, String city, String state, Integer zipCode, String country, Student idStudent) {
+		
+		this.idAdress = idAdress;
+		this.street = street;
+		this.city = city;
+		this.state = state;
+		this.zipCode = zipCode;
+		this.country = country;
+		this.idStudent = idStudent;
+	}
 
+	public Integer getIdAdress() {
+		return idAdress;
+	}
+
+	public void setIdAdress(Integer idAdress) {
+		this.idAdress = idAdress;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getState() {
+		return state;
+	}
+
+	public void setState(String state) {
+		this.state = state;
+	}
+
+	public Integer getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(Integer zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public String getCountry() {
+		return country;
+	}
+
+	public void setCountry(String country) {
+		this.country = country;
+	}
+
+	public Student getIdStudent() {
+		return idStudent;
+	}
+
+	public void setIdStudent(Student idStudent) {
+		this.idStudent = idStudent;
+	}
+
+	@Override
+	public String toString() {
+		return "Adress [idAdress=" + idAdress + ", street=" + street + ", city=" + city + ", state=" + state
+				+ ", zipCode=" + zipCode + ", country=" + country + ", idStudent=" + idStudent + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((city == null) ? 0 : city.hashCode());
+		result = prime * result + ((country == null) ? 0 : country.hashCode());
+		result = prime * result + ((idAdress == null) ? 0 : idAdress.hashCode());
+		result = prime * result + ((state == null) ? 0 : state.hashCode());
+		result = prime * result + ((street == null) ? 0 : street.hashCode());
+		result = prime * result + ((zipCode == null) ? 0 : zipCode.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Adress other = (Adress) obj;
+		if (city == null) {
+			if (other.city != null)
+				return false;
+		} else if (!city.equals(other.city))
+			return false;
+		if (country == null) {
+			if (other.country != null)
+				return false;
+		} else if (!country.equals(other.country))
+			return false;
+		if (idAdress == null) {
+			if (other.idAdress != null)
+				return false;
+		} else if (!idAdress.equals(other.idAdress))
+			return false;
+		if (state == null) {
+			if (other.state != null)
+				return false;
+		} else if (!state.equals(other.state))
+			return false;
+		if (street == null) {
+			if (other.street != null)
+				return false;
+		} else if (!street.equals(other.street))
+			return false;
+		if (zipCode == null) {
+			if (other.zipCode != null)
+				return false;
+		} else if (!zipCode.equals(other.zipCode))
+			return false;
+		return true;
+	}
+	
+	
+	
 }
