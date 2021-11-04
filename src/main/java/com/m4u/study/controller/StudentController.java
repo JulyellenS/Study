14fbdf9040114324a999a20f3b9d28884676fdc7
Marcelo Mendes <marcelo.mendes@iegaranhuns.local>
2021-11-03 22:38:28 -0300

    Criando class student controller

--- src/main/java/com/m4u/study/controller/StudentController.java
+++ src/main/java/com/m4u/study/controller/StudentController.java
@@ -1,17 +1,51 @@
 package com.m4u.study.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.m4u.study.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import com.m4u.study.model.Student;
+import org.springframework.http.MediaType;
+import java.util.List;
 
 @RestController
-@RequestMapping("/student")
 public class StudentController {
-	
-	@SuppressWarnings("unused")
-	private void sysout() {
-		System.out.println("Estou aqui!");
+	@Autowired
+	StudentService studentService;
 
-	} 
-	
+	@RequestMapping(method = RequestMethod.POST, value = "/student", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Student> cadastrarStudent(@RequestBody Student student){
+		Student studentSalvo = studentService.cadastrar(student);
+		return new ResponseEntity<>(studentSalvo, HttpStatus.CREATED);
+
+	}
+
+	@RequestMapping(method = RequestMethod.GET, value = "/student", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<List<Student>> buscarStudent(){
+		List<Student> studentEncontrado = studentService.buscarTodos();
+		return new ResponseEntity<>(studentEncontrado, HttpStatus.OK);
+
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value= "/student/{id}")
+	public ResponseEntity<Student> excluirStudent(@PathVariable Integer id){
+		Student studentEncontrado = studentService.buscarPorId(id);
+		if(studentEncontrado == null){
+			return new ResponseEntity<>(studentEncontrado, HttpStatus.NOT_FOUND);
+		}else{
+			studentService.excluir(studentEncontrado);
+			return new ResponseEntity<>( HttpStatus.OK);
+
+		}
+
+	}
+
+	@RequestMapping(method = RequestMethod.PUT, value = "/student", consumes = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<Student> alterarStudent(@RequestBody Student student){
+		Student studentAlterado = studentService.alterar(student);
+		return new ResponseEntity<>(studentAlterado, HttpStatus.OK);
+
+	}
 
 }
